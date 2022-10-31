1607b0608a8befc83ea83c47b9c305eebe0784f0
Tingxu-Max-Lan <71983537+Tingxu-Max-Lan@users.noreply.github.com>
2022-10-30 22:42:42 -0700

    added comments

--- password.c
+++ password.c
@@ -9,7 +9,10 @@
 #include <regex.h>
 #include <string.h>
 bool valid(string password);
-
+//We get a user input, and then run that input through our valid function. The valid function is a for loop that goes through each character of the string.
+//If the character we are checking is uppercase, lowercase, number, or symbol, we set the corresponding boolean to true. If all the booleans are true by the end of 
+//The for loop, the function returns true. Otherwise, it returns false.
+//We print out a message corresponding with whether the password was valid or not 
 int main(void)
 {
     string password = get_string("Enter your password: ");
@@ -23,14 +26,14 @@ int main(void)
     }
 }
 bool valid(string password){
-    char lol;
+    char temp; //this stores the current character we are checking
  int string;
     bool uppercase = false;
     bool lowercase = false;
     bool num = false;
     bool symbol = false;
     for(int i = 0; i < strlen(password); i++){
-        lol = password[i];
+        temp = password[i];
     if(isupper(lol)){
         uppercase = true;
     }
