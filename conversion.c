dc8694de349e8477076b1aebb4b7d562365086a4
Tingxu-Max-Lan <71983537+Tingxu-Max-Lan@users.noreply.github.com>
2022-11-09 11:39:14 -0800

    Add files via upload

--- conversion.c
+++ conversion.c
@@ -11,71 +11,36 @@ Ask the user for a positive integer
 We run the calc function using the user input number and bases 2,8,16
 We ask the user if they want to try again
 */
-   int mode = 1;
-int twos;
-int countBits(int n)
-{
-   int count = 1;
-   while (n)
-   {
-        count++;
-        n >>= 1; //shifts the bits to the right by one, effectly dividing by two
+int mode = 0;
+void print(int *adress){
+       for(int i = adress[0]; i >= 1;i--){
+        if (adress[i] > 9) printf("%c",adress[i] % 10 + 'A');
+        else printf("%i", adress[i]);
+        }
+         printf("\n");
     }
-    return count;
-}
-int find(int start, int base){
-    int sum = 0;
-    int nDigits = floor(log10(abs(base))) + 1;
-    for(int i = 1; i < nDigits; i++){
-        sum = sum + pow(floor(start/10^(i-1)),base^i);
-    }
-    return sum;
-}
-void print(int array[], int length){
 
-       for(int i = length; i >= 0;i--){
-             if(array[i] == 10){
-                  printf("A");
-            }else if(array[i] == 11){
-                 printf("B");
-            }else if(array[i] == 12){
-                 printf("C");
-            }else if(array[i] == 13){
-                 printf("D");
-            }else if(array[i] == 14){
-                 printf("E");
-            }else if(array[i] == 15){
-                 printf("F");
-            }else if(array[i] == 16){
-                 printf("G");
-            }else{
-        printf("%i", array[i]);}
-    }
-    printf(" \n");
-}
-void calc(int base, int num){
-    int array[32];
 
-    int i = 0;
+int * calc(int base, int num){
+    static int array[32];
+    int i = 1;
     while(num >= base){
         array[i]=num%base;
         num /= base;
         i++;
     }
+    array[0] = i;
     array[i]=num;
-    print(array,i);
+    return(array);
 }
 int main(void){
-
-        int num;
+    int num;
     do{num = get_int("enter number: ");}
     while(num < 0);
     int base;
     do{base = get_int("enter base: ");}
     while(base < 0 || base > 16);
-    calc(base,num);
-
-
+    print(calc(base,num));
     int yes;
     do{
     yes = get_int("retry? press 1 for yes, 2 for no ");
@@ -85,5 +50,4 @@ int main(void){
         main();
         mode = 0;
     }
-
 }
