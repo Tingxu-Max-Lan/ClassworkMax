273ee0707f9dfaa7be37aeb4bdb1134f328eb18c
Tingxu-Max-Lan <71983537+Tingxu-Max-Lan@users.noreply.github.com>
2023-03-22 11:55:28 -0700

    Update chess.c

--- chess.c
+++ chess.c
@@ -7,7 +7,8 @@
 #include <time.h>
 #include <unistd.h>
 #include <time.h>
-
+int state = 0;
+bool white = true;
 enum Type{
     Empty = 0,
     Pawn,
@@ -63,21 +64,96 @@ int listen(int x, int y)
     //should never happen
     return -1;
 }
-int check(int x,int y){
+int check(int *moves){
+    int y = moves[0];
+    int x = moves[1];
+    int z = moves[2];
+    int a = moves[3];
+    int piece = board[y][x].type;
     //checks whether a move is valid or not. the parameter signifies the place where the piece is.
     //returns 0 if it is valid, and 1 if it is not valid, and 2 if it has no moves, and -1 if there is no piece.
     return 0;
 }
-char *ask(void){
-    static char move[4];
+void boardstate(void){
+    for(int j = 0; j < 8; j++)
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            switch(board[j][i].type){
+                case Pawn:
+                if(board[j][i].color == Black){
+                    for(int x = 0; x<2;x++){
+                        if(!(abs(i+x > 8)>8)){
+                        if(board[j-1][i+(1-2*(x%2))].type == King && board[j-1][i+(1-2*(x%2))].color == White){
+                            state = 1;
+                        }
+                        }
+                    }
+                }else{
+                    for(int x = 0; x<2;x++){
+                        if(!(abs(i+x > 8)>8)){
+                        if(board[j+1][i+(1-2*(x%2))].type == King && board[j+1][i+(1-2*(x%2))].color == Black){
+                            state = 2;
+                        }
+                        }
+                    }
+                }
+                    break;
+                case Knight:
+                if(board[j][i].color == Black){
+                    for(int x = 0; x<4;x++){
+                        int row = ((1-2*(x%2))*2-(1-2*(x%2)));
+                        int col = (2*(-1+(2*ceil((double)(x%3)/((x%3)+1)))));
+
+                        if(!(abs(row) > 8 || abs(col) > 8)){
+                            if(board[row][col].type == King && board[row][col].color == White){
+                                state = 1;
+                            }
+                        }
+                        col = (2*(-1+(2*ceil((double)(x%3)/((x%3)+1))))-(-1+(2*ceil((double)(x%3)/((x%3)+1)))));
+                        row = ((1-2*(x%2))*2);
+                        if(!(abs(row) > 8 || abs(col) > 8)){
+                            if(board[row][col].type == King && board[row][col].color == White){
+                                state = 1;
+                            }
+                        }
+                    }
+                }else{
+                    for(int x = 0; x<4;x++){
+
+                    }
+                }
+                case Bishop:
+                    break;
+                case Rook:
+                    break;
+                case Queen:
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
+int *ask(void){
+    static int move[4];
     /*repeatedly asks for a three letter string until the user enters a valid move and returns that string
     It does this with a for loop checking if its the correct type and length and then using check()
     to make sure that it is a legal move.
     */
     return move;
 }
-int update(char *g){
-    //interprets the string passed by the listen function to update the board struct array
+int update(int *update){
+    int y = update[0];
+    int x = update[1];
+    int a = update[2];
+    int z = update[3];
+    board[z][a].type = board[x][y].type;
+    board[z][a].color = board[x][y].color;
+    board[x][y].type = Empty;
+    board[x][y].color = Blank;
+    //interprets the string passed by the ask function to update the board struct array
     //checks to see if there is a check or checkmate or staleate
     //returns 0 for no check, 1 for check, 2 for white stalemate, 3 for black stalemate, 4 for white win, 5 for black win
     //prints out the array along with other UI info like previous moves and pieces captured
